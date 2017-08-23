6ee05ce70e0c4d62e8ca6a0e597011df9ebdc390
Flantsune <30864906+Flantsune@users.noreply.github.com>
2017-08-23 15:32:21 -0300

    Add files via upload

---
+++ MovimentoCaixa.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoCaixa : MonoBehaviour
+{
+    public float velocidade = 10;
+    Rigidbody2D rigid;
+    public float pular = 100;
+
+    public float agachado = 2;
+    public float correr = 9;
+    bool chao;
+    public LayerMask chaoLayer;
+    SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float moveX = Input.GetAxis("Horizontal");
+		rigid.velocity = (new Vector2(moveX * velocidade, rigid.velocity.y));
+
+        
+		if (Input.GetKeyDown(KeyCode.Space) && chao)
+        {
+            rigid.AddForce (new Vector2(0, pular));
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && chao)
+        {
+            rigid.velocity = new Vector2(moveX * agachado, rigid.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            rigid.velocity = new Vector2(moveX * correr, rigid.velocity.y);
+        }
+
+        if (moveX < 0) 
+		{
+			spriteRenderer.flipX = true;
+		}
+		else if (moveX > 0)
+		{
+			spriteRenderer.flipX = false;
+		}
+    }
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.8f, chaoLayer);
+        Debug.DrawRay(transform.position, -Vector2.up, Color.red);
+        if (hit.collider != null)
+        {
+            chao = true;
+        }
+        else
+        {
+            chao = false;
+        }
+    }
+}
---
+++ MovimentoFormiga.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoFormiga : MonoBehaviour {
+    public int velocidade = 1;
+    public int velocidadeataca = 2;
+    public int tempo1 = 1;
+    public int tempo2 = 20;
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigid;
+    public LayerMask CaixaLeite;
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        float moveX = velocidade;
+        rigid.velocity = (new Vector2(moveX, rigid.velocity.y));
+        if (moveX < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveX > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        tempo1 = tempo1 + 1;
+        if (tempo1 >= 30)
+        {
+            tempo1 = 1;
+        }
+        if (Divisivel(tempo1,tempo2))
+        {
+            rigid.velocity = (new Vector2(moveX*-1, rigid.velocity.y));
+        }       
+    }
+    public void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, 2, CaixaLeite);
+        Debug.DrawRay(transform.position, Vector2.left, Color.red);
+        if (hit.collider != null)
+        {
+			velocidade = -2;
+        }
+        else
+        {
+            velocidade = 1;
+        }
+    }
+    public bool Divisivel(int tempo1, int tempo2)
+    {
+        return (tempo1 % tempo2) == 0;
+    }
+}
